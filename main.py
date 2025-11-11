961d0e112c1e8b0d4cab48c7a82312f0cc496067
TamirhaG <96215456+TamirhaG@users.noreply.github.com>
2025-11-11 05:57:58 +0000

    Actualiza main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from src.data_validation import validate_dataset, plot_eda
 from src.preprocessing import preprocess_data, balance_dataset
 from src.model_training import train_models
 from src.evaluation import evaluate_models
+from src.metrics_visualization import visualize_metrics
+
 
 def main():
     df = validate_dataset()
@@ -9,7 +11,9 @@ def main():
     df_clean = preprocess_data()
     # df_bal = balance_dataset(df_clean)  # opcional por RAM
     metrics = train_models()
-    evaluate_models()  # ⬅️ nuevo bloque
+    evaluate_models()
+    visualize_metrics()
+
 
 if __name__ == "__main__":
     main()
